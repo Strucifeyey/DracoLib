909aa13dbef2486aef6e485f233b188ccb1acdb3
Strucifeyey <strucifeyeyt@gmail.com>
2025-03-11 22:55:14 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -137,7 +137,7 @@ function Library:ApplyTextStroke(Inst)
 
     Library:Create('UIStroke', {
         Color = Color3.new(0, 0, 0);
-        Thickness = 1;
+        Thickness = 0;
         LineJoinMode = Enum.LineJoinMode.Miter;
         Parent = Inst;
     });
