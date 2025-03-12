efa09e497987921f49d0c61bd045d2f51a74ab10
Strucifeyey <strucifeyeyt@gmail.com>
2025-03-11 22:48:39 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -30,7 +30,7 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
+    MainColor = Color3.fromRGB(14, 15, 20);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(204, 31, 35);
     OutlineColor = Color3.fromRGB(50, 50, 50);
